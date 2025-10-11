520b6e85d18c0f26f6a792d3d30c1a1a16836b7b
Naoki Watanabe <asterisk37n@gmail.com>
2025-10-11 12:46:22 +0900

    Add Chapter 9

---
+++ src/chap9/ClassInterpreter.java
@@ -0,0 +1,22 @@
+package chap9;
+import java.io.FileReader;
+import java.io.IOException;
+import stone.ClassParser;
+import stone.Lexer;
+import stone.ParseException;
+import chap6.BasicInterpreter;
+import chap7.NestedEnv;
+import chap8.Natives;
+
+public class ClassInterpreter extends BasicInterpreter {
+    public static void main(String[] args) throws ParseException, IOException {
+    if (args.length < 1) {
+        System.err.println("Usage: java NativeInterpreter <source file>");
+        System.exit(1);
+    }
+    String path = args[0];
+    Lexer lexer = new Lexer(new FileReader(path));
+    run(new ClassParser(),
+        new Natives().environment(new NestedEnv()), lexer);
+    }
+}
